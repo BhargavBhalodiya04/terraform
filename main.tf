f59886b1594ac22b43e9f87f80383f534f12fe67
Bhargav Bhalodiya <your-email@gmail.com>
2026-02-23 17:34:33 +0530

    key file change

--- main.tf
+++ main.tf
@@ -1,7 +1,11 @@
 resource "aws_instance" "my_server" {
   ami           = "ami-0f5ee92e2d63afc18"
   instance_type = "t2.micro"
-  key_name      = "All-key"
+  key_name      = "All-Key"   # <-- Correct case
+
+  lifecycle {
+    prevent_destroy = true
+  }
 
   tags = {
     Name = "GitHubAction-Terraform-Server"
